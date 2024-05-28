8b1f6729838434310546dd340d2d5dae8313917a
khodembn <mbmr2089@gmail.com>
2024-05-28 11:06:06 +0330

    2

---
+++ project.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+void stub();
+void login();
+bool stup();
+void questionMenu();
+void tagMenu();
+void userMenu();
+
+int main()
+{
+    char choice;
+
+    while (true)
+    {
+        cout << "* Login(L)" << endl;
+        cout << "* view the Questions(v)" << endl;
+
+        cin >> choice;
+        switch (choice)
+        {
+        case 'l':
+        case 'L':
+            if (stup())
+                login();
+            else
+                stub();
+            break;
+        case 'v':
+        case 'V':
+            stub();
+            break;
+        }
+        system("cls");
+    }
+    return 0;
+}
+void stub()
+{
+}
+bool stup()
+{
+    return 1;
+}
+void login()
+{
+    cout << "----------------------------------" << endl
+         << "Hello" << stup() << '!'
+         << "----------------------------------" << endl;
+    char choice;
+
+    while (true)
+    {
+        cout << "* Question menu(Q)" << endl;
+        cout << "* Tag menu(T)" << endl;
+        cout << "* User menu(U)" << endl;
+        cin >> choice;
+        switch (choice)
+        {
+        case 'Q':
+        case 'q':
+            questionMenu();
+            break;
+        case 't':
+        case 'T':
+            tagMenu();
+            break;
+        case 'u':
+        case 'U':
+            userMenu();
+            break;
+        }
+        system("cls");
+    }
+}
---
+++ struct.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+class FourChoice
+{
+public:
+    FourChoice give(string, string, string, string, string) private : string A, B, C, D;
+    char answer;
+};
