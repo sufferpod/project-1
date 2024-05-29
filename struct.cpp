db326306c749209fc9bc74b99df599d3a1879d44
khodembn <mbmr2089@gmail.com>
2024-05-29 12:22:50 +0330

    2

--- struct.cpp
+++ struct.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+#include "question.h"
+#include "structs.h"
 using namespace std;
 
-class FourChoice
+class FourChoice : public Question
 {
 public:
-    FourChoice(string A, string B, string C, string D, char answer) : A(A), B(B), C(C), D(D), answer(answer) {}
+    FourChoice(string Qu, DateTime ti, string A, string B, string C, string D, char answer) : Question(Qu, "FourChoice", ti), A(A), B(B), C(C), D(D), answer(answer) {}
+    void print()
+    {
+
+        cout << "A) " << A << endl
+             << "B) " << B << endl
+             << "C) " << C << endl
+             << "D) " << D << endl;
+    }
+    static FourChoice *create(string question, DataTime creatAt, User user, string A, string B, string C, string D, char answer);
 
 private:
     string A, B, C, D;
     char answer;
 };
+FourChoice *FourChoice::static create(string question, DataTime creatAt, User user, string A, string B, string C, string D, char answer)
+{
+}
