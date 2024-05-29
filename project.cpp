0fa185836fe66f13e2017046c2c0ddba69588499
sufferpod <sadra759@gmail.com>
2024-05-30 02:03:37 +0330

    tierd

--- project.cpp
+++ project.cpp
@@ -131,7 +131,7 @@ bool User::checkAuth(string un, string pa)
 class Auth
 {
 private:
-    static User *auth;
+    inline static User *auth = NULL;
 
 public:
     static User *login(string username, string password);
@@ -166,7 +166,7 @@ User *Auth::whoami()
 {
     return auth;
 }
-/// resolve error
+
 class Tag
 {
 private:
@@ -195,21 +195,20 @@ void Tag::print()
     cout << title << endl;
 }
 
-Tag *tags[100] = {NULL};
+Tag *Gtags[100] = {NULL};
 
 void Tag::printAll()
 {
-    cout << " tags: ";
-    for (int i = 0; tags[i] != NULL; i++)
+    cout << "tags: ";
+    for (int i = 0; Gtags[i] != NULL; i++)
     {
-        cout << tags[i]->title << ',';
+        cout << Gtags[i]->title << ',';
     }
     cout << endl;
 }
-// todo
+
 class Question
 {
-private:
 public:
     Question(string Qu, string ty, DateTime ti, User us) : question(Qu), type(ty), createdAt(ti), user(us) {}
     void publish() { isPublished = true; }
@@ -239,19 +238,14 @@ void Question::addTag(Tag *tag)
         tags[i] = tag;
 }
 
+Question *questions[100] = {NULL};
+
 class FourChoice : public Question
 {
 public:
-    void printAll() {} // todo
-    FourChoice(string Qu, DateTime ti, User us, string A, string B, string C, string D, char answer) : Question(Qu, "FourChoice", ti, us), A(A), B(B), C(C), D(D), answer(answer) {}
-    void print()
-    {
-
-        cout << "A) " << A << endl
-             << "B) " << B << endl
-             << "C) " << C << endl
-             << "D) " << D << endl;
-    }
+    FourChoice(string Qu, DateTime ti, User us, string a, string b, string c, string d, char ans);
+    void print();
+    void printAll();
     FourChoice *edit(string question, DateTime createdAt, User user, string A, string B, string C, string D, char answer);
     static FourChoice *create(string question, DateTime createdAt, User user, string A, string B, string C, string D, char answer);
 
@@ -260,19 +254,83 @@ private:
     char answer;
 };
 
+FourChoice::FourChoice(string Qu, DateTime ti, User us, string a, string b, string c, string d, char ans) : Question(Qu, "four-choice", ti, us), A(a), B(b), C(c), D(d), answer(ans) {}
+
+void FourChoice::print()
+{
+    cout << "Question: " << question << endl
+         << "A) " << A << endl
+         << "B) " << B << endl
+         << "C) " << C << endl
+         << "D) " << D << endl;
+    cout << "Created at: " << createdAt.year << '/' << createdAt.month << '/' << createdAt.day
+         << ' ' << createdAt.hour << ':' << createdAt.minute << ':' << createdAt.second << endl
+         << "by user ";
+    user.print();
+    cout << "Tags: ";
+    for (int i = 0; tags[i] != NULL; i++)
+    {
+        cout << tags[i] << ',';
+    }
+    cout << endl;
+    if (isPublished)
+        cout << "This question is published\n";
+    else
+        cout << "This question is not published\n";
+}
+
+void FourChoice::printAll()
+{
+    for (int i = 0; questions[i] != NULL; i++)
+    {
+        if (questions[i]->type == "four-choice")
+        {
+            questions[i]->print();
+        }
+    }
+}
+
 FourChoice *FourChoice::edit(string question, DateTime createdAt, User user, string A, string B, string C, string D, char answer)
 {
-    this->A = A;
-    this->B = B;
-    this->C = C;
-    this->D = D;
-    this->answer = answer;
-    return new FourChoice(question, createdAt, user, A, B, C, D, answer);
+    bool flag = false;
+    for (int i = 0; Auth::whoami()->permissions[i] = NULL; i++)
+    {
+        if (whoami()->permissions[i]->title == "edit-four-choice-question")
+            flag = true;
+    }
+    if (flag)
+    {
+        this->A = A;
+        this->B = B;
+        this->C = C;
+        this->D = D;
+        this->answer = answer;
+        return new FourChoice(question, createdAt, user, A, B, C, D, answer);
+    }
+    else
+        return NULL;
 }
+
 FourChoice *FourChoice::create(string question, DateTime createdAt, User user, string A, string B, string C, string D, char answer)
 {
-    FourChoice *x = new FourChoice(question, createdAt, user, A, B, C, D, answer);
-    return x;
+    bool flag = false;
+    for (int i = 0; Auth::whoami()->permissions[i] = NULL; i++)
+    {
+        if (whoami()->permissions[i]->title == "add-four-choice-question")
+            flag = true;
+    }
+    if (flag)
+    {
+        FourChoice *x = new FourChoice(question, createdAt, user, A, B, C, D, answer);
+        int i = 0;
+        for (; questions[i] != NULL; i++)
+        {
+            ;
+        }
+        questions[i] = x;
+        return x;
+    }
+    return NULL;
 }
 
 class Descriptive : public Question
@@ -305,7 +363,6 @@ bool stup();
 void questionMenu();
 void tagMenu();
 void userMenu();
-Question *questions[100] = {NULL};
 
 int main()
 {
