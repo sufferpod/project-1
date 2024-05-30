5f4dc0fda976af1279f5ad029b8da55519b6f658
sufferpod <sadra759@gmail.com>
2024-05-30 22:50:01 +0330

    pls be the last one

--- project.cpp
+++ project.cpp
@@ -62,6 +62,7 @@ public:
     static User *create(string n, string un, string pa);
     void addpermission(Permission *permission);
     void print();
+    static void printAll();
     bool checkAuth(string un, string pa);
     bool checkPermTi(string);
     string myName();
@@ -70,28 +71,23 @@ User *users[100] = {NULL};
 User *User::create(string n, string un, string pa)
 {
     User *x = new User(n, un, pa);
-    bool flag = false;
     for (int i = 0; x->permissions[i] != NULL; i++)
     {
         if (x->permissions[i]->viewTitle() == "add-user")
         {
-            flag = true;
-        }
-    }
-    if (flag)
-    {
-        int i = 0;
-        for (; users[i] != NULL; i++)
-        {
-            if (x->username == users[i]->username)
+            int i = 0;
+            for (; users[i] != NULL; i++)
             {
-                break;
+                if (x->username == users[i]->username)
+                {
+                    break;
+                }
             }
-        }
-        if (users[i] == NULL)
-        {
-            users[i] = x;
-            return x;
+            if (users[i] == NULL)
+            {
+                users[i] = x;
+                return x;
+            }
         }
     }
     return NULL;
@@ -118,6 +114,16 @@ void User::print()
     cout << "Name: " << name
          << "\nUser name: " << username
          << "Password: " << password << endl;
+}
+
+void User::printAll()
+{
+    for (int i = 0; users[i] != NULL; i++)
+    {
+        cout << "ID: " << i << endl;
+        users[i]->print();
+        cout << endl;
+    }
 }
 
 bool User::checkAuth(string un, string pa)
@@ -199,9 +205,17 @@ public:
     static void printAll();
 };
 
+Tag *Gtags[100] = {NULL};
 Tag *Tag::create(string ti)
 {
-    return new Tag(ti);
+    Tag *temp = new Tag(ti);
+    int i = 0;
+    for (; Gtags[i] != NULL; i++)
+    {
+        ;
+    }
+    Gtags[i] = temp;
+    return temp;
 }
 
 string Tag::veiwTitle()
@@ -214,14 +228,14 @@ void Tag::print()
     cout << title;
 }
 
-Tag *Gtags[100] = {NULL};
-
 void Tag::printAll()
 {
     cout << "tags: ";
     for (int i = 0; Gtags[i] != NULL; i++)
     {
-        cout << Gtags[i]->title << ',';
+        cout << "ID: " << i << '=';
+        Gtags[i]->print();
+        cout << endl;
     }
     cout << endl;
 }
@@ -234,6 +248,8 @@ public:
     void unpublish() { isPublished = false; }
     virtual void print() = 0;
     virtual void printAll() = 0;
+    virtual Question *edit(string, DateTime, User, string, string, string, string, char) = 0;
+    virtual Question *edit(string, DateTime, User) = 0;
     void addTag(Tag *);
     string TypeBack();
 
@@ -271,7 +287,8 @@ public:
     FourChoice(string Qu, DateTime ti, User us, string a, string b, string c, string d, char ans);
     void print();
     void printAll();
-    FourChoice *edit(string question, DateTime createdAt, User user, string A, string B, string C, string D, char answer);
+    Question *edit(string question, DateTime createdAt, User user, string A, string B, string C, string D, char answer) override;
+    Question *edit(string, DateTime, User);
     static FourChoice *create(string question, DateTime createdAt, User user, string A, string B, string C, string D, char answer);
 
 private:
@@ -310,13 +327,14 @@ void FourChoice::printAll()
     {
         if (questions[i]->TypeBack() == "four-choice")
         {
-            questions[i]->print();
             cout << "ID : " << i << endl;
+            questions[i]->print();
         }
     }
 }
 
-FourChoice *FourChoice::edit(string question, DateTime createdAt, User user, string A, string B, string C, string D, char answer)
+Question *FourChoice::edit(string, DateTime, User) { return NULL; }
+Question *FourChoice::edit(string question, DateTime createdAt, User user, string A, string B, string C, string D, char answer)
 {
     if (Auth::whoami()->checkPermTi("edit-four-choice-question"))
     {
@@ -356,7 +374,8 @@ public:
     void print();
     void printAll();
     static Descriptive *create(string question, DateTime createdAt, User user);
-    Descriptive *edit(string question, DateTime createdAt, User user);
+    Question *edit(string, DateTime, User, string, string, string, string, char);
+    Question *edit(string question, DateTime createdAt, User user) override;
 
 private:
     string answer;
@@ -395,8 +414,8 @@ void Descriptive::printAll()
     {
         if (questions[i]->TypeBack() == "descriptive")
         {
-            questions[i]->print();
             cout << "ID : " << i << endl;
+            questions[i]->print();
         }
     }
 }
@@ -418,7 +437,8 @@ Descriptive *Descriptive::create(string question, DateTime createdAt, User user)
     return NULL;
 }
 
-Descriptive *Descriptive::edit(string question, DateTime createdAt, User user)
+Question *Descriptive::edit(string, DateTime, User, string, string, string, string, char) { return NULL; }
+Question *Descriptive::edit(string question, DateTime createdAt, User user)
 {
     if (Auth::whoami()->checkPermTi("edit-descriptive-question"))
     {
@@ -455,14 +475,6 @@ int main()
     return 0;
 }
 
-void stub()
-{
-}
-bool stup()
-{
-    return 1;
-}
-
 void loginFMenu()
 {
     char choice;
@@ -477,12 +489,15 @@ void loginFMenu()
         case 'l':
         case 'L':
         {
-            cout << "Enter username and password on separate lines:\n";
             cin.ignore();
-            string username, password;
-            getline(cin, username);
-            getline(cin, password);
-            Auth::login(username, password);
+            string n, un, pa;
+            cout << "Enter Name:\n";
+            getline(cin, n);
+            cout << "Enter UserName:\n";
+            getline(cin, un);
+            cout << "Enter Password:\n";
+            getline(cin, pa);
+            Auth::login(un, pa);
             loginTMenu();
             break;
         }
@@ -539,20 +554,20 @@ void loginTMenu()
 
 void questionMenu()
 {
-    cout << "Question Menu\n";
-    cout << "\t* Create Descriptive Question(C)\n"
-         << "\t* Edit Descriptive Question(E)\n"
-         << "\t* List of Descriptive Questions with ID(D)\n"
-         << "\t* Print Descriptive Question(P)\n"
-         << "\t* Create FourChoice Question(U)\n"
-         << "\t* Edit FourChoice Question(H)\n"
-         << "\t* List of FourChoice Questions with ID(F)\n"
-         << "\t* Print FourChoice Question(K)\n"
-         << "\t* view All Questions(V)\n"
-         << "\t* Exit(X)\n";
     char choice;
     while (true)
     {
+        cout << "Question Menu\n";
+        cout << "\t* Create Descriptive Question(C)\n"
+             << "\t* Edit Descriptive Question(E)\n"
+             << "\t* List of Descriptive Questions with ID(D)\n"
+             << "\t* Print Descriptive Question(P)\n"
+             << "\t* Create FourChoice Question(U)\n"
+             << "\t* Edit FourChoice Question(H)\n"
+             << "\t* List of FourChoice Questions with ID(F)\n"
+             << "\t* Print FourChoice Question(K)\n"
+             << "\t* view All Questions(V)\n"
+             << "\t* Exit(X)\n";
         cin >> choice;
         switch (choice)
         {
@@ -582,8 +597,7 @@ void questionMenu()
             DateTime temp;
             cout << "\nEnter time of Creation in this format Y/M/D H:M:S: \n";
             cin >> temp.year >> temp.month >> temp.day >> temp.hour >> temp.minute >> temp.second;
-            // questions[ID]->edit(question, temp, *Auth::whoami());
-            // todo
+            questions[ID]->edit(question, temp, *Auth::whoami());
             break;
         }
         case 'd':
@@ -625,13 +639,43 @@ void questionMenu()
         }
         case 'h':
         case 'H':
+        {
+            int ID;
+            cout << "Enter the ID of the Question you want to edit: \n";
+            cin >> ID;
+            cin.ignore();
+            string question;
+            cout << "Enter the Question: \n";
+            getline(cin, question);
+            string a, b, c, d;
+            cout << "Enter Options(one on each line): \n";
+            getline(cin, a);
+            getline(cin, b);
+            getline(cin, c);
+            getline(cin, d);
+            char ans;
+            cout << "Enter the correct answer's letter: ";
+            cin >> ans;
+            DateTime temp;
+            cout << "\nEnter time of Creation in this format Y/M/D H:M:S: \n";
+            cin >> temp.year >> temp.month >> temp.day >> temp.hour >> temp.minute >> temp.second;
+            questions[ID]->edit(question, temp, *Auth::whoami(), a, b, c, d, ans);
             break;
+        }
         case 'f':
         case 'F':
+            printFQ();
             break;
         case 'k':
         case 'K':
+        {
+            int ID;
+            cout << " Enter the ID of the Question you want to print: \n";
+            cin >> ID;
+            questions[ID]->print();
             break;
+            break;
+        }
         case 'v':
         case 'V':
             printAllQ();
@@ -643,8 +687,95 @@ void questionMenu()
     }
 }
 
-void tagMenu() {}
-void userMenu() {}
+void tagMenu()
+{
+    char choice;
+    while (true)
+    {
+        cout << "Tag Menu\n";
+        cout << "\t*List of Tags with ID(L)\n"
+             << "\t*Create New Tag(T)\n"
+             << "\t* Exit(X)\n";
+
+        cin >> choice;
+        switch (choice)
+        {
+        case 'l':
+        case 'L':
+            Tag::printAll();
+            break;
+        case 't':
+        case 'T':
+        {
+            string temp;
+            cout << "Enter the Tag you want to add: \n";
+            cin.ignore();
+            getline(cin, temp);
+            Tag::create(temp);
+            break;
+        }
+        }
+        if (choice == 'x' || choice == 'X')
+            break;
+        system("cls");
+    }
+}
+
+void userMenu()
+{
+    char choice;
+    while (true)
+    {
+        cout << "User Menu\n";
+        cout << "\t*List of Users with ID(L)\n"
+             << "\t*Create New User(U)\n"
+             << "\t*Add permission to a user(P)\n"
+             << "\t* Exit(X)\n";
+
+        cin >> choice;
+        switch (choice)
+        {
+        case 'l':
+        case 'L':
+            User::printAll();
+            break;
+        case 'u':
+        case 'U':
+        {
+            cin.ignore();
+            string n, un, pa;
+            cout << "Enter Name:\n";
+            getline(cin, n);
+            cout << "Enter UserName:\n";
+            getline(cin, un);
+            cout << "Enter Password:\n";
+            getline(cin, pa);
+            User::create(n, un, pa);
+            break;
+        }
+        case 'p':
+        case 'P':
+        {
+            int ID;
+            cout << "enter the ID of the User you want to add permission to: \n";
+            cin >> ID;
+            int p;
+            cout << "What permission do you want to add?\n"
+                 << "add-descriptive-question(1)\n"
+                 << "add-four-choice-question(2)\n"
+                 << "edit-descriptive-question(3)\n"
+                 << "edit-four-choice-question(4)\n"
+                 << "add-user(5)\n";
+            cin >> p;
+            users[ID]->addpermission(Gpermissions[p]);
+            break;
+        }
+        }
+        if (choice == 'x' || choice == 'X')
+            break;
+        system("cls");
+    }
+}
 
 void printDQ()
 {
